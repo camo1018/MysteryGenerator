da7206043fb3eb7ee737f1210d632e557ead4eec
Paul Park <camo1018@yahoo.com>
2013-05-18 18:50:11 -0400

    Initial Commit

---
+++ Source/MysteryGenerator/src/pp/GeneratorCore/Driver.java
@@ -0,0 +1,21 @@
+package pp.GeneratorCore;
+
+public class Driver {
+	
+	/**
+	 * Instantiate a Story Generator Driver for starting the story generation. 
+	 */
+	public Driver() {
+	
+		// TODO: Implementation
+		
+		// Load Story Data From XML
+		
+	}
+	
+	
+	
+	
+	
+
+}
